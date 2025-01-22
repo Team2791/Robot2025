4dc99b35da667bda636111c4fbec344febc5c483
Angad Tendulkar <angad.tendulkar@gmail.com>
2025-01-22 15:21:21 -0500

    advantagekit: deps and setup

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,22 +4,83 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+import org.littletonrobotics.urcl.URCL;
+
+import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.constants.AdvantageConstants;
+import frc.robot.constants.BuildConstants;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
 	final RobotContainer container;
 
 	Command autoCommand;
 
 	public Robot() {
-		container = new RobotContainer();
+		// setup logger constants
+		Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+		Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+		Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+		Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+		Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+
+		switch (BuildConstants.DIRTY) {
+			case 0:
+				Logger.recordMetadata("GitStatus", "All changes committed");
+				break;
+			case 1:
+				Logger.recordMetadata("GitStatus", "Uncomitted changes");
+				break;
+			default:
+				Logger.recordMetadata("GitStatus", "Unknown");
+				break;
+		}
+
+		// setup logger data receivers
+		switch (AdvantageConstants.Modes.kCurrent) {
+			case Real:
+				Logger.addDataReceiver(new WPILOGWriter());
+				Logger.addDataReceiver(new NT4Publisher());
+
+				break;
+			case Sim:
+				Logger.addDataReceiver(new NT4Publisher());
+				break;
+			default:
+				String logfile = LogFileUtil.findReplayLog();
+
+				setUseTiming(false);
+				Logger.setReplaySource(new WPILOGReader(logfile));
+				Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logfile, "sim")));
+				break;
+		}
+
+		// register rev hardware logger
+		Logger.registerURCL(URCL.startExternal());
+
+		// start logger
+		Logger.start();
+
+		this.container = new RobotContainer();
 	}
 
 	@Override
 	public void robotPeriodic() {
+		// Performance: give ourselves very high priority
+		Threads.setCurrentThreadPriority(true, 99);
+
+		// Run the robot for a tick
 		CommandScheduler.getInstance().run();
+
+		// High prio no longer needed
+		Threads.setCurrentThreadPriority(false, 10);
 	}
 
 	@Override
