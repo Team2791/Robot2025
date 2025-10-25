eb8779466d8f31ebf61ffc11b9bb1209cb813600
Angad Tendulkar <angad@angad.page>
2025-10-25 09:42:02 -0400

    offset gyro

--- src/main/java/frc/robot/subsystems/drivetrain/gyro/NavX.java
+++ src/main/java/frc/robot/subsystems/drivetrain/gyro/NavX.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems.drivetrain.gyro;
 
-import static edu.wpi.first.units.Units.*;
-
 import com.studica.frc.AHRS;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.Angle;
@@ -9,18 +7,20 @@ import edu.wpi.first.wpilibj.Notifier;
 import frc.robot.constants.ControlConstants;
 import frc.robot.constants.IOConstants;
 import frc.robot.util.Alerter;
+
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.locks.ReentrantLock;
 
+import static edu.wpi.first.units.Units.*;
+
 public class NavX extends GyroIO {
     final AHRS gyro;
-    Rotation2d offset = new Rotation2d();
-
-    final Notifier thread = new Notifier(this::read);
     final Queue<Double> readings = new ArrayBlockingQueue<>(20);
     final ReentrantLock lock = new ReentrantLock();
     final double rate = 100;
+    Rotation2d offset = Rotation2d.kPi;
+    final Notifier thread = new Notifier(this::read);
 
     public NavX() {
         this.gyro = new AHRS(IOConstants.Drivetrain.kGyroPort, (byte) rate);
